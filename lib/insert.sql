c70dd3be4b90a1a1e549673078ebf5260af6cb75
mariamrashid <giannarashid@gmail.com>
2025-08-23 23:53:48 +0300

    Fix syntax error in sql_queries.py and add correct SQL queries

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 5, 'M', 'Brown', 'Calm', 1),
+('Rowdy', 7, 'M', 'Black', 'Aggressive', 1),
+('Tabitha', 4, 'F', 'Gray', 'Curious', 1),
+('Sergeant Brown', 8, 'M', 'Brown', 'Brave', 1),
+('Melissa', 6, 'F', 'White', 'Sweet', 1),
+('Grinch', 9, 'M', 'Green', 'Grumpy', 0),
+('Wendy', 3, 'F', 'Blonde', 'Shy', 1),
+(NULL, 2, 'F', 'Black', 'Quiet', 1);
